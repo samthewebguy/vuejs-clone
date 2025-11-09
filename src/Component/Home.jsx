9dfe46ffd0894ea87f846cf7012bddb5a2221af5
Samthewebguy <reachsamthewebguy@gmail.com>
2025-11-09 21:42:48 +0100

    Update play icon

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react'
-import PlayIcon from '../assets/Images/playicon.svg'
+import PlayIcon from '../assets/Images/playIcon.svg'
 import ArrowForward from '../assets/Images/arrowForward.svg'
 import LinkIcon from '../assets/Images/Link-icon.svg'
 import PlatinumSponsors from './PlatinumSponsors'
